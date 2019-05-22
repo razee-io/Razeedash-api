4c32cccf3882d060c74ac463750514c9d6ed2509
Adam King <rak@us.ibm.com>
2019-05-22 14:12:47 -0400

    Add new /api/install/cluster route to handle installing and maintaining razee on a new cluster

--- app/routes/install/index.js
+++ app/routes/install/index.js
@@ -72,6 +72,30 @@ router.get('/kapitan', asyncHandler(async (req, res, next) => {
   }
 }));
 
+router.get('/cluster', asyncHandler(async (req, res, next) => {
+  const orgKey = req.orgKey;
+  var razeeapiUrl = `${req.protocol}://${req.get('host')}/api/v2`;
+  const kptn_url = 'https://github.com/razee-io/kapitan-delta/releases/latest/download/resource.yaml';
+  const remoteResource_url = 'https://github.com/razee-io/RemoteResource/releases/latest/download/resource.yaml';
+  try {
+    const inventory = await readFile(`${__dirname}/cluster.yaml`, 'utf8');
+    const rr = await request.get(remoteResource_url);
+    const kptn = await request.get(kptn_url);
+    const view = {
+      RAZEEDASH_URL: razeeapiUrl,
+      RAZEEDASH_ORG_KEY: Buffer.from(orgKey).toString('base64'),
+      REMOTE_RESOURCE: rr,
+      KAPITAN: kptn
+    };
+    const configYaml = Mustache.render(inventory, view);
+    res.setHeader('content-type', 'application/yaml');
+    return res.status(200).send(configYaml);
+  } catch (e) {
+    req.log.error(e);
+    next(e);
+  }
+}));
+
 router.get('/kapitan/:component', asyncHandler(async (req, res, next) => {
   const kptn_url = `https://github.com/razee-io/${req.params.component}/releases/latest/download/resource.yaml`;
   try {
