fdcdbccfbda59df0657e2e3e812d402331ed363d
Geoff Corey <gwcorey@us.ibm.com>
2019-05-30 10:43:13 -0400

    add test 'should return 205 if cluster does not exist and is dirty'

--- app/routes/v2/clusters.tests.js
+++ app/routes/v2/clusters.tests.js
@@ -65,6 +65,7 @@ describe('clusters', () => {
 
     });
   });
+
   describe('POST /:cluster_id', () => {
     it('should return 200 if cluster does not exist and not dirty', async () => {
       // Setup
@@ -105,6 +106,50 @@ describe('clusters', () => {
 
       assert.equal(response.statusCode, 200);
       assert.equal(response._getData(), 'Thanks for the update');
+
+    });
+  });
+
+  describe('POST /:cluster_id', () => {
+    it('should return 205 if cluster does not exist and is dirty', async () => {
+      // Setup
+      let addUpdateCluster = v2.__get__('addUpdateCluster');
+      await db.collection('clusters').insertOne({ org_id: 1, cluster_id: 'testUpdateOne205', dirty: 1  });
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/testUpdateOne205', params: {
+          cluster_id: 'testUpdateOne205'
+        },
+        org: {
+          _id: 1
+        },
+        body: {
+          kube_version: {
+            major: '1',
+            minor: '13',
+            gitVersion: 'v1.13.6+IKS',
+            gitCommit: 'ac5f7341d5d0ce8ea8f206ba5b030dc9e9d4cc97',
+            gitTreeState: 'clean',
+            buildDate: '2019-05-09T13:26:51Z',
+            goVersion: 'go1.11.5',
+            compiler: 'gc',
+            platform: 'linux/amd64'
+          }
+        },
+        db: db
+      });
+
+      var response = httpMocks.createResponse();
+      // Test
+      let next = (error) => {
+        assert.equal(error, null);
+      };
+
+
+      await addUpdateCluster(request, response, next);
+
+      assert.equal(response.statusCode, 205);
+      assert.equal(response._getData(), 'Please resync');
 
     });
   });
