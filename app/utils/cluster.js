14c6b84c8d6a89a0c1ad3659d2a04300de280e09
Dale R. Hille <dalehille@gmail.com>
2019-06-11 11:10:12 -0400

    create annotations.razee_io_commit_sha keys in searchableData

--- app/utils/cluster.js
+++ app/utils/cluster.js
@@ -94,14 +94,21 @@ const buildSearchableDataForResource = (obj) => {
   ];
   let out = {};
   _.each(searchableAttrs, (searchableAttr) => {
-    let saveAsName = (searchableAttr.name || searchableAttr.attrPath).replace(/[^a-z0-9_]/gi, '_');
-    let valToSave = _.get(obj, searchableAttr.attrPath, null);
-    if(_.isObject(valToSave) || _.isArray(valToSave)){
-      valToSave = cleanObjKeysForMongo(valToSave);
+    if(searchableAttr.name === 'annotations') {
+      const annotations = _.get(obj, searchableAttr.attrPath, null);
+      for(let key in annotations) {
+        const sanitizedName = 'annotations.' + key.replace(/[^a-z0-9_]/gi, '_');
+        out[sanitizedName] = annotations[key];
+      }
+    } else {
+      let saveAsName = (searchableAttr.name || searchableAttr.attrPath).replace(/[^a-z0-9_]/gi, '_');
+      let valToSave = _.get(obj, searchableAttr.attrPath, null);
+      if(_.isObject(valToSave) || _.isArray(valToSave)){
+        valToSave = cleanObjKeysForMongo(valToSave);
+      }
+      _.set(out, saveAsName, valToSave);
     }
-    _.set(out, saveAsName, valToSave);
   });
-
   return out;
 };
 
