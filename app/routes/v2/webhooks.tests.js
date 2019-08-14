71032f1b895c38c5eb6ebc3ba7c76c94a1d2cca1
Geoff Corey <gwcorey@us.ibm.com>
2019-08-14 10:43:27 -0400

    work around mongo-mock to get _id from beforeEach inserts

--- app/routes/v2/webhooks.tests.js
+++ app/routes/v2/webhooks.tests.js
@@ -21,20 +21,39 @@ const log = require('../../log').log;
 
 const rewire = require('rewire');
 let v2 = rewire('./webhooks');
-const { WEBHOOK_TRIGGER_CLUSTER } = require('../../utils/webhook');
+const { WEBHOOK_TRIGGER_IMAGE, WEBHOOK_TRIGGER_CLUSTER } = require('../../utils/webhook');
 let db = {};
-let req = {};
+let webhook1 = {};
+let webhook2 = {};
 
 describe('webhooks', () => {
   beforeEach((done) => {
     mongodb.max_delay = 0;
     const MongoClient = mongodb.MongoClient;
+    MongoClient.persist = false;
     MongoClient.connect('someconnectstring', {}, async (err, database) => {
       try {
-        const Clusters = database.collection('clusters');
-        database.collection('webhooks');
-        database.collection('images');
         db = database;
+        const Clusters = db.collection('clusters');
+        const Webhooks = db.collection('webhooks');
+        const Images = db.collection('images');
+        await Webhooks.insertOne({
+          org_id: 1,
+          kind: 'image',
+          trigger: WEBHOOK_TRIGGER_IMAGE,
+          field: 'name',
+          filter: '(quay.io\\/mynamespace)',
+          service_url: 'https://somewhere.else/check'
+        });        
+        webhook1 = await Webhooks.findOne({'kind':'image'}); // work around for mongo-mock to get _id
+        await Webhooks.insertOne({
+          org_id: 1,
+          kind: 'Deployment',
+          trigger: WEBHOOK_TRIGGER_CLUSTER,
+          cluster_id: 'myclusterid',
+          service_url: 'https://integrationtest.elsewhere/run'
+        });
+        webhook2 = await Webhooks.findOne({'kind':'Deployment'});
         await Clusters.insertOne({
           org_id: 1,
           cluster_id: 'myclusterid',
@@ -52,13 +71,52 @@ describe('webhooks', () => {
         });
         done();
       } catch (err) {
+        console.log('pants');
         console.log(err);
+        done();
       }
     });
   });
 
   afterEach(() => {
     db.close();
+  });
+
+  describe('addCallbackResult', () => {
+    it('should throw db error', async () => {
+      // Setup
+      let addCallbackResult = v2.__get__('addCallbackResult');
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/',
+        org: {
+          _id: 1
+        },
+        log: log,
+        body: {
+          webhook_id: 'mywebhookid',
+          url: 'https://i.imgur.com/jR0LYTx.jpg',
+          description: 'test passed',
+          link: 'http://myfakeservice',
+          status: 'info'
+        }
+      });
+      // eslint-disable-next-line require-atomic-updates
+      request.db = {
+        collection: () => { throw new Error('oops'); },
+        close: () => { return; }
+      };
+      var response = httpMocks.createResponse();
+      // Test
+      let nextCalled = false;
+      let next = (err) => {
+        assert.equal(err.message, 'oops');
+        nextCalled = true;
+      };
+
+      await addCallbackResult(request, response, next);
+      assert.equal(nextCalled, true);
+    });
   });
 
   describe('addWebhook', () => {
@@ -81,7 +139,7 @@ describe('webhooks', () => {
       });
       // eslint-disable-next-line require-atomic-updates
       request.db = {
-        collection: () => { throw new Error('oops'); },
+        collection: () => { throw new Error('oops'); }, // DB Error
         close: () => { return; }
       };
       var response = httpMocks.createResponse();
@@ -96,7 +154,7 @@ describe('webhooks', () => {
       assert.equal(nextCalled, true);
     });
 
-    it('success 201', async () => {
+    it('cluster trigger 201', async () => {
       // Setup
       let addWebhook = v2.__get__('addWebhook');
       var request = httpMocks.createRequest({
@@ -107,9 +165,9 @@ describe('webhooks', () => {
         },
         log: log,
         body: {
-          cluster_id: 'myclusterid',
-          trigger: WEBHOOK_TRIGGER_CLUSTER,
           kind: 'Deployment',
+          trigger: WEBHOOK_TRIGGER_CLUSTER,
+          cluster_id: 'myclusterid',
           service_url: 'http://myfakeservice'
         },
         db: db
@@ -125,6 +183,36 @@ describe('webhooks', () => {
 
       assert.equal(response.statusCode, 201);
     });
+
+    it('image trigger 201', async () => {
+      // Setup
+      let addWebhook = v2.__get__('addWebhook');
+      var request = httpMocks.createRequest({
+        method: 'POST',
+        url: '/',
+        org: {
+          _id: 1
+        },
+        log: log,
+        body: {
+          kind: 'Image',
+          trigger: WEBHOOK_TRIGGER_IMAGE,
+          service_url: 'http://myfakeservice'
+        },
+        db: db
+      });
+
+      var response = httpMocks.createResponse();
+      // Test
+      let next = (err) => {
+        assert.equal(err.message, null);
+      };
+
+      await addWebhook(request, response, next);
+
+      assert.equal(response.statusCode, 201);
+    });
+
     it('should return 404', async () => {
       // Setup
       let addWebhook = v2.__get__('addWebhook');
@@ -160,7 +248,7 @@ describe('webhooks', () => {
       let deleteWebhook = v2.__get__('deleteWebhook');
       var request = httpMocks.createRequest({
         method: 'DELETE', params: {
-          webhook_id: 'webhookRoutesWebhookId1'
+          webhook_id: webhook1._id
         },
         org: {
           _id: 1
@@ -169,7 +257,7 @@ describe('webhooks', () => {
       });
       // eslint-disable-next-line require-atomic-updates
       request.db = {
-        collection: () => { throw new Error('oops'); },
+        collection: () => { throw new Error('oops'); }, // DB Error
         close: () => { }
       };
       var response = httpMocks.createResponse();
@@ -181,26 +269,17 @@ describe('webhooks', () => {
       };
 
       await deleteWebhook(request, response, next);
-      request.db.close();
       assert.equal(nextCalled, true);
     });
 
     it('should return 201', async () => {
       // Setup
-      const Webhooks = db.collection('webhooks');
-      await Webhooks.insertOne({
-        _id: 'webhookRoutesWebhookId1',
-        org_id: 'image1',
-        kind: 'image',
-        trigger: 'image',
-        field: 'name',
-        filter: '(quay.io\\/mynamespace)',
-        service_url: 'https://somewhere.else/check'
-      });
+
       let deleteWebhook = v2.__get__('deleteWebhook');
       var request = httpMocks.createRequest({
-        method: 'DELETE', params: {
-          webhook_id: 'webhookRoutesWebhookId1'
+        method: 'DELETE', 
+        params: {
+          webhook_id: webhook2._id
         },
         url: '/',
         org: {
