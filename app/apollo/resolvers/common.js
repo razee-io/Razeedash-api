96d3c2bb07dda3b9c265c9904822402dab21089a
Ying Wang <yingwang@us.ibm.com>
2020-03-18 16:50:56 -0400

    update

--- app/apollo/resolvers/common.js
+++ app/apollo/resolvers/common.js
@@ -25,7 +25,7 @@ const whoIs = me => {
 // Validate is user is authorized for the requested action.
 // Throw exception if not.
 const validAuth = async (me, org_id, action, type, models, queryName, req_id, logger) => {
-  if (me === null || !(await models.User.isAuthorized(me, org_id, action, type))) {
+  if (me === null || !(await models.User.isAuthorized(me, org_id, action, type, null))) {
     logger.error({req_id, me: whoIs(me), org_id, action, type}, `AuthenticationError - ${queryName}`);
     throw new AuthenticationError(
       `You are not allowed to ${action} on ${type} under organization ${org_id} for the query ${queryName}.`,
