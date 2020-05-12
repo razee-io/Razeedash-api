3305ffc7e864e7cb73bcd8ec253592fab3d27914
Ying Wang <yingwang@us.ibm.com>
2020-05-11 22:39:00 -0400

    update

--- app/apollo/index.js
+++ app/apollo/index.js
@@ -96,6 +96,7 @@ const createApolloServer = () => {
   }
   logger.info(customPlugins, 'Apollo server custom plugin are loaded.');
   const server = new ApolloServer({
+    introspection: process.env.NODE_ENV !== 'production',
     plugins: customPlugins,
     tracing: process.env.GRAPHQL_ENABLE_TRACING === 'true',
     playground: process.env.NODE_ENV !== 'production',
