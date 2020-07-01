f9e4454d3c9d0aa23d2f4eb7aab29e2aa6c96099
Ying Wang <yingwang@us.ibm.com>
2020-07-01 12:03:32 -0400

    more camel case changes

--- app/apollo/resolvers/group.js
+++ app/apollo/resolvers/group.js
@@ -67,7 +67,7 @@ const groupResolvers = {
               groups: group.name,
             },
           },
-          { $project: { name: 1, uuid: 1, groups: 1, version: 1, channel: 1 , orgId: '$org_id', channelUuid: '$channel_uuid', versionUuid: '$version_uuid', owner: 1, created: 1, updated: 1 } },
+          { $project: { _id: 0, name: 1, uuid: 1, groups: 1, version: 1, channel: 1 , orgId: '$org_id', channelUuid: '$channel_uuid', versionUuid: '$version_uuid', owner: 1, created: 1, updated: 1 } },
         ]);
         const clusterCount = await models.Cluster.count({ org_id: org_id, 'groups.uuid': group.uuid });
         const clusters = await models.Cluster.aggregate([
@@ -77,7 +77,7 @@ const groupResolvers = {
               'groups.uuid': group.uuid,
             },
           },
-          { $project: { clusterId: '$cluster_id', groups: 1, registration: 1, regState: '$reg_state', orgId: '$org_id', created: 1, updated: 1 } },
+          { $project: { _id: 0, clusterId: '$cluster_id', 'groups.uuid': 1, 'groups.name': 1, registration: 1, regState: '$reg_state', orgId: '$org_id', created: 1, updated: 1 } },
         ]);
         
         group.owner = owners[group.owner];
@@ -107,7 +107,7 @@ const groupResolvers = {
               groups: group.name,
             },
           },
-          { $project: { name: 1, uuid: 1, groups: 1, version: 1, channel: 1 , orgId: '$org_id', channelUuid: '$channel_uuid', versionUuid: '$version_uuid', owner: 1, created: 1, updated: 1 } },
+          { $project: { _id: 0, name: 1, uuid: 1, groups: 1, version: 1, channel: 1 , orgId: '$org_id', channelUuid: '$channel_uuid', versionUuid: '$version_uuid', owner: 1, created: 1, updated: 1 } },
         ]);
         const clusterCount = await models.Cluster.count({ org_id: org_id, 'groups.uuid': group.uuid });
         const clusters = await models.Cluster.aggregate([
@@ -117,7 +117,7 @@ const groupResolvers = {
               'groups.uuid': group.uuid,
             },
           },
-          { $project: { clusterId: '$cluster_id', groups: 1, registration: 1, regState: '$reg_state', orgId: '$org_id', created: 1, updated: 1 } },
+          { $project: { _id: 0, clusterId: '$cluster_id', 'groups.uuid': 1, 'groups.name': 1, registration: 1, regState: '$reg_state', orgId: '$org_id', created: 1, updated: 1 } },
         ]);
         
         group.owner = owners[group.owner];
