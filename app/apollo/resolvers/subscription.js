b35294d134b7ef43a48264ddf4e95ceba8fd5ede
Keith Morgan <keithmorganIBM@users.noreply.github.com>
2021-02-22 11:27:10 -0500

    try 3

--- app/apollo/resolvers/subscription.js
+++ app/apollo/resolvers/subscription.js
@@ -473,10 +473,11 @@ const subscriptionResolvers = {
 
       var success = false;
       try{
-        var subscription = await models.Subscription.findOne({ org_id, uuid });
-        //const conditions = await getGroupConditionsIncludingEmpty(me, org_id, ACTIONS.READ, 'name', queryName, context);
-        //logger.debug({req_id, user: whoIs(me), org_id, conditions }, `${queryName} group conditions are...`);
+        //var subscription = await models.Subscription.findOne({ org_id, uuid });
+        const conditions = await getGroupConditionsIncludingEmpty(me, org_id, ACTIONS.READ, 'name', queryName, context);
+        logger.debug({req_id, user: whoIs(me), org_id, conditions }, `${queryName} group conditions are...`);
         //var subscription = await models.Subscription.findOne({ org_id, uuid, ...conditions }, {}).lean({ virtuals: true });
+        var subscription = await models.Subscription.findOne({ org_id, uuid, ...conditions }, {});
 
         if(!subscription){
           throw  new NotFoundError(context.req.t('Subscription uuid "{{uuid}}" not found.', {'uuid':uuid}), context);
