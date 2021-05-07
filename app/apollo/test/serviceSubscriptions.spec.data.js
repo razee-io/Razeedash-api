75a4861b3c5b114904bd1c784f2a11fb85ceb594
Michael Topchiev <michael.topchiev@ibm.com>
2021-05-07 18:08:07 -0400

    Fixed lint issues

--- app/apollo/test/serviceSubscriptions.spec.data.js
+++ app/apollo/test/serviceSubscriptions.spec.data.js
@@ -14,157 +14,157 @@
  * limitations under the License.
  */
 
-'use strict'
+'use strict';
 
 const { models } = require('../models');
 const { v4: UUID } = require('uuid');
 const ObjectId = require('mongoose').Types.ObjectId;
 
 const createTestData = async () => {
-    const org01Data = {
-        "type": "local",
-        "name": "org_01"
-    };
-    const org01 = await models.Organization.createLocalOrg(org01Data);
+  const org01Data = {
+    'type': 'local',
+    'name': 'org_01'
+  };
+  const org01 = await models.Organization.createLocalOrg(org01Data);
 
-    const org2Data = {
-        "type": "local",
-        "name": "org_2"
-    };
-    const org02 = await models.Organization.createLocalOrg(org2Data);
+  const org2Data = {
+    'type': 'local',
+    'name': 'org_2'
+  };
+  const org02 = await models.Organization.createLocalOrg(org2Data);
 
-    const user01Data = {
-        "username": "user01",
-        "email": "user01@us.ibm.com",
-        "password": "password123",
-        "orgName": "org_01",
-        "role": "ADMIN"
-    };
-    const user01 = await models.User.createUser(models, user01Data);
+  const user01Data = {
+    'username': 'user01',
+    'email': 'user01@us.ibm.com',
+    'password': 'password123',
+    'orgName': 'org_01',
+    'role': 'ADMIN'
+  };
+  const user01 = await models.User.createUser(models, user01Data);
 
-    const user02Data = {
-        "username": "user02",
-        "email": "user02@us.ibm.com",
-        "password": "password123",
-        "orgName": "org_02",
-        "role": "ADMIN"
-    };
-    const user02 = await models.User.createUser(models, user02Data);
+  const user02Data = {
+    'username': 'user02',
+    'email': 'user02@us.ibm.com',
+    'password': 'password123',
+    'orgName': 'org_02',
+    'role': 'ADMIN'
+  };
+  const user02 = await models.User.createUser(models, user02Data);
 
-    const channelData = {
-        _id: UUID(),
-        org_id: org01._id,
+  const channelData = {
+    _id: UUID(),
+    org_id: org01._id,
+    uuid: UUID(),
+    name: 'channel_01_name',
+    versions: [
+      {
         uuid: UUID(),
-        name: "channel_01_name",
-        versions: [
-            {
-                uuid: UUID(),
-                name: "channelVersion_01_name"
-            },
-            {
-                uuid: UUID(),
-                name: "channelVersion_02_name"
-            }
-        ]
-    }
-    await models.Channel.create(channelData);
-
-    const versionData = {
-        _id: UUID(),
-        org_id: org01._id,
-        uuid: channelData.versions[0].uuid,
-        channel_id: channelData.uuid,
-        channel_name: channelData.name,
-        name: "v1"
-    }
-    await models.DeployableVersion.create(versionData);
-
-    // Cluster 1 is in org01
-    const cluster1Data = {
-        org_id: org01._id,
-        cluster_id: 'cluster_01',
-        metadata: {
-            kube_version: {
-                major: '1',
-                minor: '16',
-                gitVersion: '1.99',
-                gitCommit: 'abc',
-                gitTreeState: 'def',
-                buildDate: 'a_date',
-                goVersion: '1.88',
-                compiler: 'some compiler',
-                platform: 'linux/amd64',
-            },
-        },
-        registration: { name: 'cluster-1' }
-    }
-    await models.Cluster.create(cluster1Data);
-
-    // Cluster 2 is in org02
-    const cluster2Data = {
-        org_id: org02._id,
-        cluster_id: 'cluster_02',
-        metadata: {
-            kube_version: {
-                major: '1',
-                minor: '16',
-                gitVersion: '1.99',
-                gitCommit: 'abc',
-                gitTreeState: 'def',
-                buildDate: 'a_date',
-                goVersion: '1.88',
-                compiler: 'some compiler',
-                platform: 'linux/amd64',
-            },
-        },
-        registration: { name: 'cluster-2' }
-    }
-    await models.Cluster.create(cluster2Data);
-
-    // Service subscription
-    const ssid1 = UUID();
-    const serSub1 = await models.ServiceSubscription.create({
-        _id: ssid1,
-        org_id: org01._id,
-        uuid: ssid1,
-        name: 'service_subscription_01_name',
-        owner: user02._id,
-        clusterId: cluster2Data.cluster_id,
-        channel_uuid: channelData.uuid,
-        channelName: channelData.name,
-        version: channelData.versions[0].name,
-        version_uuid: channelData.versions[0].uuid // 1st version
-    });
-
-    // User subscription
-    const userSub1 = await models.Subscription.create({
-        _id: 'user-subscription-id-1',
-        org_id: org01._id,
+        name: 'channelVersion_01_name'
+      },
+      {
         uuid: UUID(),
-        name: "subscription_01_name",
-        owner: user01._id,
-        clusterId: cluster2Data.cluster_id,
-        channel_uuid: channelData.uuid,
-        channel: channelData.name,
-        version: channelData.versions[1].name,
-        version_uuid: channelData.versions[1].uuid // 2nd version
-    });
+        name: 'channelVersion_02_name'
+      }
+    ]
+  };
+  await models.Channel.create(channelData);
 
-    const resource = await models.Resource.create({
-        _id: new ObjectId('aaaabbbbccccddddeeeeffff'),
-        org_id: org02._id,
-        cluster_id: cluster2Data.cluster_id,
-        selfLink: 'any_selfLink',
-        hash: 'any_hash',
-        deleted: false,
-        data: 'any_data',
-        searchableData: { 'annotations["deploy_razee_io_clustersubscription"]': serSub1.uuid, key02: 'value-02' },
-        searchableDataHash: 'some random hash',
-    });
-//searchableData.
-    console.log(`\t Created service subscriptions test data`);
+  const versionData = {
+    _id: UUID(),
+    org_id: org01._id,
+    uuid: channelData.versions[0].uuid,
+    channel_id: channelData.uuid,
+    channel_name: channelData.name,
+    name: 'v1'
+  };
+  await models.DeployableVersion.create(versionData);
 
-    return { org01, channelData, cluster2Data: cluster2Data, serSub1, userSub1 }
-}
+  // Cluster 1 is in org01
+  const cluster1Data = {
+    org_id: org01._id,
+    cluster_id: 'cluster_01',
+    metadata: {
+      kube_version: {
+        major: '1',
+        minor: '16',
+        gitVersion: '1.99',
+        gitCommit: 'abc',
+        gitTreeState: 'def',
+        buildDate: 'a_date',
+        goVersion: '1.88',
+        compiler: 'some compiler',
+        platform: 'linux/amd64',
+      },
+    },
+    registration: { name: 'cluster-1' }
+  };
+  await models.Cluster.create(cluster1Data);
+
+  // Cluster 2 is in org02
+  const cluster2Data = {
+    org_id: org02._id,
+    cluster_id: 'cluster_02',
+    metadata: {
+      kube_version: {
+        major: '1',
+        minor: '16',
+        gitVersion: '1.99',
+        gitCommit: 'abc',
+        gitTreeState: 'def',
+        buildDate: 'a_date',
+        goVersion: '1.88',
+        compiler: 'some compiler',
+        platform: 'linux/amd64',
+      },
+    },
+    registration: { name: 'cluster-2' }
+  };
+  await models.Cluster.create(cluster2Data);
+
+  // Service subscription
+  const ssid1 = UUID();
+  const serSub1 = await models.ServiceSubscription.create({
+    _id: ssid1,
+    org_id: org01._id,
+    uuid: ssid1,
+    name: 'service_subscription_01_name',
+    owner: user02._id,
+    clusterId: cluster2Data.cluster_id,
+    channel_uuid: channelData.uuid,
+    channelName: channelData.name,
+    version: channelData.versions[0].name,
+    version_uuid: channelData.versions[0].uuid // 1st version
+  });
+
+  // User subscription
+  const userSub1 = await models.Subscription.create({
+    _id: 'user-subscription-id-1',
+    org_id: org01._id,
+    uuid: UUID(),
+    name: 'subscription_01_name',
+    owner: user01._id,
+    clusterId: cluster2Data.cluster_id,
+    channel_uuid: channelData.uuid,
+    channel: channelData.name,
+    version: channelData.versions[1].name,
+    version_uuid: channelData.versions[1].uuid // 2nd version
+  });
+
+  await models.Resource.create({
+    _id: new ObjectId('aaaabbbbccccddddeeeeffff'),
+    org_id: org02._id,
+    cluster_id: cluster2Data.cluster_id,
+    selfLink: 'any_selfLink',
+    hash: 'any_hash',
+    deleted: false,
+    data: 'any_data',
+    searchableData: { 'annotations["deploy_razee_io_clustersubscription"]': serSub1.uuid, key02: 'value-02' },
+    searchableDataHash: 'some random hash',
+  });
+
+  console.log('\t Created service subscriptions test data');
+
+  return { org01, channelData, cluster2Data: cluster2Data, serSub1, userSub1 };
+};
 
 module.exports = createTestData;
--- app/apollo/test/serviceSubscriptions.spec.js
+++ app/apollo/test/serviceSubscriptions.spec.js
@@ -14,14 +14,16 @@
  * limitations under the License.
  */
 
-'use strict'
+'use strict';
 
 const { AUTH_MODEL } = require('../models/const');
 const { signInUser } = require(`./testHelper.${AUTH_MODEL}`);
 
-// Service subscriptions require super-user, 
+// Service subscriptions require super-user,
 // which is implemented only in local auth model
-if (AUTH_MODEL !== 'local') return;
+if (AUTH_MODEL !== 'local') {
+  return; // eslint-disable-line
+}
 
 const createTestData = require('./serviceSubscriptions.spec.data');
 
@@ -44,163 +46,162 @@ const resourceApi = resourceFunc(graphqlUrl);
 const queries = serviceSubscriptionQueries(graphqlUrl);
 
 const orgAdminKey = 'huge-secret';
-const user01cred = { email: "user01@us.ibm.com", password: "password123" };
-const user02cred = { email: "user02@us.ibm.com", password: "password123", orgAdminKey };
+const user01cred = { email: 'user01@us.ibm.com', password: 'password123' };
+const user02cred = { email: 'user02@us.ibm.com', password: 'password123', orgAdminKey };
 
 describe('Service subscription graphql test suite', () => {
-    let mongoServer;
-    let myApollo;
-    let testData;
-    let user01token;
-    let user02superUserToken;
+  let mongoServer;
+  let myApollo;
+  let testData;
+  let user01token;
+  let user02superUserToken;
 
-    before(async () => {
-        process.env.NODE_ENV = 'test';
-        process.env.ORG_ADMIN_KEY = orgAdminKey;
-        mongoServer = new MongoMemoryServer();
-        const mongoUrl = await mongoServer.getUri();
-        console.log(`\tCluster.js in memory test mongodb url is ${mongoUrl}`);
+  before(async () => {
+    process.env.NODE_ENV = 'test';
+    process.env.ORG_ADMIN_KEY = orgAdminKey;
+    mongoServer = new MongoMemoryServer();
+    const mongoUrl = await mongoServer.getUri();
+    console.log(`\tCluster.js in memory test mongodb url is ${mongoUrl}`);
 
-        myApollo = await apollo({
-            mongo_url: mongoUrl,
-            graphql_port: graphqlPort,
-        });
-
-        testData = await createTestData();
-        user01token = await signInUser(models, resourceApi, user01cred);
-        user02superUserToken = await signInUser(models, resourceApi, user02cred);
-    }); // before
-
-    after(async () => {
-        await myApollo.stop(myApollo);
-        GraphqlPubSub.deleteInstance();
-        await mongoServer.stop();
-    }); // after
-
-    it('Get subscription type returns SERVICE', async () => {
-        const { data: result } =
-            await queries.subscriptionType(user02superUserToken, {
-                orgId: testData.org01._id,
-                id: testData.serSub1.uuid,
-            });
-        printResults(result);
-        expect(result.data.subscriptionType).to.equal('SERVICE');
+    myApollo = await apollo({
+      mongo_url: mongoUrl,
+      graphql_port: graphqlPort,
     });
 
-    it('Get subscription type returns USER', async () => {
-        const { data: result } =
-            await queries.subscriptionType(user01token, {
-                orgId: testData.org01._id,
-                id: testData.userSub1.uuid,
-            });
-        printResults(result);
-        expect(result.data.subscriptionType).to.equal('USER');
-    });
+    testData = await createTestData();
+    user01token = await signInUser(models, resourceApi, user01cred);
+    user02superUserToken = await signInUser(models, resourceApi, user02cred);
+  }); // before
 
-    it('Add a service subscription fails when non-superuser calls the api', async () => {
-        const { data: result } =
-            await queries.addServiceSubscription(user01token, {
-                orgId: testData.org01._id,
-                name: 'a_random_name',
-                clusterId: testData.cluster2Data.cluster_id,
-                channelUuid: testData.channelData.uuid,
-                versionUuid: testData.channelData.versions[0].uuid,
-            });
-        printResults(result);
-        expect(result.errors[0].message).to.include('not allowed');
-    })
+  after(async () => {
+    await myApollo.stop(myApollo);
+    GraphqlPubSub.deleteInstance();
+    await mongoServer.stop();
+  }); // after
 
-    it('Add a service subscription', async () => {
-        const { data: result } =
-            await queries.addServiceSubscription(user02superUserToken, {
-                orgId: testData.org01._id,
-                name: 'my awesome new service subscription',
-                clusterId: testData.cluster2Data.cluster_id,
-                channelUuid: testData.channelData.uuid,
-                versionUuid: testData.channelData.versions[0].uuid,
-            });
-        printResults(result);
-        expect(result.data.addServiceSubscription).to.be.an('string');
-    })
+  it('Get subscription type returns SERVICE', async () => {
+    const { data: result } =
+      await queries.subscriptionType(user02superUserToken, {
+        orgId: testData.org01._id,
+        id: testData.serSub1.uuid,
+      });
+    printResults(result);
+    expect(result.data.subscriptionType).to.equal('SERVICE');
+  });
 
-    it('Get service subscriptions should return two items', async () => {
-        const { data: result } =
-            await queries.serviceSubscriptions(user02superUserToken, {
-                orgId: testData.org01._id
-            });
-        printResults(result); // second item is added by one of the previous tests
-        expect(result.data.serviceSubscriptions.length).to.equal(2);
-        expect(result.data.serviceSubscriptions
-            .find(i => i.ssid === testData.serSub1._id)).to.be.an('object');
-    });
+  it('Get subscription type returns USER', async () => {
+    const { data: result } =
+      await queries.subscriptionType(user01token, {
+        orgId: testData.org01._id,
+        id: testData.userSub1.uuid,
+      });
+    printResults(result);
+    expect(result.data.subscriptionType).to.equal('USER');
+  });
 
-    it('Get all subscriptions should return three items', async () => {
-        const { data: result } =
-            await queries.allSubscriptions(user02superUserToken, {
-                orgId: testData.org01._id
-            });
-        printResults(result); // two existing and one added by one of the previous tests
-        expect(result.data.subscriptions.length).to.equal(3);
-        const ss = result.data.subscriptions.find(i => i.uuid === testData.serSub1.uuid);
-        expect(ss.subscriptionType).to.equal('ServiceSubscription');
-        expect(ss.remoteResources[0].cluster.clusterId).to.equal(testData.cluster2Data.cluster_id);
-    });
+  it('Add a service subscription fails when non-superuser calls the api', async () => {
+    const { data: result } =
+      await queries.addServiceSubscription(user01token, {
+        orgId: testData.org01._id,
+        name: 'a_random_name',
+        clusterId: testData.cluster2Data.cluster_id,
+        channelUuid: testData.channelData.uuid,
+        versionUuid: testData.channelData.versions[0].uuid,
+      });
+    printResults(result);
+    expect(result.errors[0].message).to.include('not allowed');
+  });
 
-    it('Get service subscription should return correct object', async () => {
-        const { data: result } =
-            await queries.serviceSubscription(user02superUserToken, {
-                orgId: testData.org01._id,
-                ssid: testData.serSub1._id
-            });
-        printResults(result);
-        expect(result.data.serviceSubscription.ssid).to.equal(testData.serSub1._id);
-        expect(result.data.serviceSubscription.owner.name).to.equal('user02');
-    });
+  it('Add a service subscription', async () => {
+    const { data: result } =
+      await queries.addServiceSubscription(user02superUserToken, {
+        orgId: testData.org01._id,
+        name: 'my awesome new service subscription',
+        clusterId: testData.cluster2Data.cluster_id,
+        channelUuid: testData.channelData.uuid,
+        versionUuid: testData.channelData.versions[0].uuid,
+      });
+    printResults(result);
+    expect(result.data.addServiceSubscription).to.be.an('string');
+  });
 
-    it('Edit service subscription should update the object', async () => {
-        const newName = 'updated service subscription';
-        const { data: result } =
-            await queries.editServiceSubscription(user02superUserToken, {
-                orgId: testData.org01._id,
-                ssid: testData.serSub1._id,
-                name: newName,
-                channelUuid: testData.serSub1.channelUuid,
-                versionUuid: testData.serSub1.versionUuid
-            });
-        printResults(result);
-        expect(result.data.editServiceSubscription).to.equal(testData.serSub1._id);
-        const ss = await models.ServiceSubscription.findById(testData.serSub1._id);
-        expect(ss.name).to.equal(newName);
-    });
+  it('Get service subscriptions should return two items', async () => {
+    const { data: result } =
+      await queries.serviceSubscriptions(user02superUserToken, {
+        orgId: testData.org01._id
+      });
+    printResults(result); // second item is added by one of the previous tests
+    expect(result.data.serviceSubscriptions.length).to.equal(2);
+    expect(result.data.serviceSubscriptions
+      .find(i => i.ssid === testData.serSub1._id)).to.be.an('object');
+  });
 
-    it('Remove service subscription should remove the object', async () => {
-        const newName = 'updated service subscription';
-        const { data: result } =
-            await queries.removeServiceSubscription(user02superUserToken, {
-                orgId: testData.org01._id,
-                ssid: testData.serSub1._id
-            });
-        printResults(result);
-        expect(result.data.removeServiceSubscription).to.equal(testData.serSub1._id);
-        const ss = await models.ServiceSubscription.findById(testData.serSub1._id);
-        expect(ss).to.be.an('null');
-    });
+  it('Get all subscriptions should return three items', async () => {
+    const { data: result } =
+      await queries.allSubscriptions(user02superUserToken, {
+        orgId: testData.org01._id
+      });
+    printResults(result); // two existing and one added by one of the previous tests
+    expect(result.data.subscriptions.length).to.equal(3);
+    const ss = result.data.subscriptions.find(i => i.uuid === testData.serSub1.uuid);
+    expect(ss.subscriptionType).to.equal('ServiceSubscription');
+    expect(ss.remoteResources[0].cluster.clusterId).to.equal(testData.cluster2Data.cluster_id);
+  });
 
-    it('Should not be able to remove version if it is used in a service subscription', async () => {
-        const { data: result } =
-            await queries.removeChannelVersion(user01token, {
-                orgId: testData.org01._id,
-                uuid: testData.channelData.versions[0].uuid
-            });
-        printResults(result);
-        expect(result.errors[0].message).to.include('depend on this');
-    });
+  it('Get service subscription should return correct object', async () => {
+    const { data: result } =
+      await queries.serviceSubscription(user02superUserToken, {
+        orgId: testData.org01._id,
+        ssid: testData.serSub1._id
+      });
+    printResults(result);
+    expect(result.data.serviceSubscription.ssid).to.equal(testData.serSub1._id);
+    expect(result.data.serviceSubscription.owner.name).to.equal('user02');
+  });
+
+  it('Edit service subscription should update the object', async () => {
+    const newName = 'updated service subscription';
+    const { data: result } =
+      await queries.editServiceSubscription(user02superUserToken, {
+        orgId: testData.org01._id,
+        ssid: testData.serSub1._id,
+        name: newName,
+        channelUuid: testData.serSub1.channelUuid,
+        versionUuid: testData.serSub1.versionUuid
+      });
+    printResults(result);
+    expect(result.data.editServiceSubscription).to.equal(testData.serSub1._id);
+    const ss = await models.ServiceSubscription.findById(testData.serSub1._id);
+    expect(ss.name).to.equal(newName);
+  });
+
+  it('Remove service subscription should remove the object', async () => {
+    const { data: result } =
+      await queries.removeServiceSubscription(user02superUserToken, {
+        orgId: testData.org01._id,
+        ssid: testData.serSub1._id
+      });
+    printResults(result);
+    expect(result.data.removeServiceSubscription).to.equal(testData.serSub1._id);
+    const ss = await models.ServiceSubscription.findById(testData.serSub1._id);
+    expect(ss).to.be.an('null');
+  });
+
+  it('Should not be able to remove version if it is used in a service subscription', async () => {
+    const { data: result } =
+      await queries.removeChannelVersion(user01token, {
+        orgId: testData.org01._id,
+        uuid: testData.channelData.versions[0].uuid
+      });
+    printResults(result);
+    expect(result.errors[0].message).to.include('depend on this');
+  });
 });
 
 function printResults(result) {
-    if (result.errors) {
-        console.log('\tApi error: ' + JSON.stringify(result.errors));
-    } else {
-        console.log("\tApi result: " + JSON.stringify(result.data));
-    }
+  if (result.errors) {
+    console.log('\tApi error: ' + JSON.stringify(result.errors));
+  } else {
+    console.log('\tApi result: ' + JSON.stringify(result.data));
+  }
 }
