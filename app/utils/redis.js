e82c96d9f943b9700771afd5d83939e8cd01e405
Ryan Graham <rmgraham@us.ibm.com>
2021-09-21 15:07:59 -0400

    made ioredis use tls info from env

--- app/utils/redis.js
+++ app/utils/redis.js
@@ -1,4 +1,5 @@
 const Redis = require('ioredis');
+const fs = require('fs');
 // const RedisLock = require('ioredis-lock');
 
 let client;
@@ -6,11 +7,17 @@ let client;
 
 const createClient = ()=>{
   const url = process.env.REDIS_PUBSUB_URL || 'redis://127.0.0.1:6379/0';
-  client = new Redis(url, {
-    tls: {
-      rejectUnauthorized: false,
-    }
-  });
+  let tlsOption;
+  if(process.env.REDIS_CERTIFICATE_PATH){
+    tlsOption = { ca: [fs.readFileSync(process.env.REDIS_CERTIFICATE_PATH)] };
+  }
+  else{
+    tlsOption = { rejectUnauthorized: false };
+  }
+  const options = {
+    tls: tlsOption,
+  };
+  client = new Redis(url, options);
   return client;
 };
 
