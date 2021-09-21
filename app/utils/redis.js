899a6721dbb7de80392370fe5fffdcb0afd7e3c8
Ryan Graham <rmgraham@us.ibm.com>
2021-09-21 14:59:24 -0400

    potentially fixed redis bug

--- app/utils/redis.js
+++ app/utils/redis.js
@@ -6,7 +6,11 @@ let client;
 
 const createClient = ()=>{
   const url = process.env.REDIS_PUBSUB_URL || 'redis://127.0.0.1:6379/0';
-  client = new Redis(url);
+  client = new Redis(url, {
+    tls: {
+      rejectUnauthorized: false,
+    }
+  });
   return client;
 };
 
