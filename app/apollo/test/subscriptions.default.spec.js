4d352c929fba3a99c1a0e4b8107bbdd1c401fc7b
Paul Carroll <carrolp@us.ibm.com>
2021-11-10 16:36:15 -0500

    adapt to mongodb-memory-server breaking changes

--- app/apollo/test/subscriptions.default.spec.js
+++ app/apollo/test/subscriptions.default.spec.js
@@ -203,8 +203,8 @@ const getOrgKey = async () => {
 describe('subscriptions graphql test suite', () => {
   before(async () => {
     process.env.NODE_ENV = 'test';
-    mongoServer = new MongoMemoryServer();
-    const mongoUrl = await mongoServer.getConnectionString();
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUrl = mongoServer.getUri();
     console.log(`    cluster.js in memory test mongodb url is ${mongoUrl}`);
 
     myApollo = await apollo({ mongo_url: mongoUrl, graphql_port: graphqlPort, });
