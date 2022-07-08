6124bde6f3326d1a6ef6449327a6c63b95250304
Paul Carroll <carrolp@us.ibm.com>
2022-07-08 15:53:36 -0400

    linting

--- app/apollo/models/const.js
+++ app/apollo/models/const.js
@@ -96,8 +96,9 @@ const DIRECTIVE_LIMITS = {
   The error messages emitted suggest only "alphabets, numbers, underscore and hyphen" are allowed, but this pattern does not accurately enforce that.
   Consider adding '`\[\]\\\/*^. as additional invalid chars for identifiers, but until refactored thoroughly this will negatively affect other values.
   E.g. ConfigurationVersion "type" attribute value "application/yaml" needs to contain a "/" and "description" attributes can be more freeform.
+  A switch to an ALLOWLIST rather than a BLOCKLIST would also be more robust in the future.
   */
-  INVALID_PATTERN: /^\s|[<>:;\*,$%&!@()}{"#\t\n\r]{1,}|\s$/,
+  INVALID_PATTERN: /^\s|[<>:;*,$%&!@()}{"#\t\n\r]{1,}|\s$/,
 };
 
 // console.log('NODE_ENV: ' + config.util.getEnv('NODE_ENV') + `, DIRECTIVE_LIMITS: ${JSON.stringify(DIRECTIVE_LIMITS)}`);
