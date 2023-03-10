fb58488de1643ae43faea30730547e8f401b1f15
Paul Carroll <carrolp@us.ibm.com>
2023-03-09 19:56:36 -0500

    swagger cleanup, gql channel query fix

--- local-dev/api/restGetChild.sh
+++ local-dev/api/restGetChild.sh
@@ -5,12 +5,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TYPE=${1:-channel}
 ID=${2:-pChannelId}
 CHILD_TYPE=${3:-version}
-CHILD_ID=${4:-pVersionId}
+CHILD_ID=/${4:-pVersionId}
 RAZEE_ORG_ID=${5:-${RAZEE_ORG_ID}}
 
 RAZEE_REST_URL=${RAZEE_REST_URL:-http://localhost:3333/api/v3}
 
-echo "Getting ${TYPE}s/${ID}/${CHILD_TYPE}s/${CHILD_ID} in orgId ${RAZEE_ORG_ID} by GET to ${RAZEE_REST_URL}"
+echo "Getting ${TYPE}s/${ID}/${CHILD_TYPE}s${CHILD_ID} in orgId ${RAZEE_ORG_ID} by GET to ${RAZEE_REST_URL}"
 
 [ ! -z "${RAZEE_USER_TOKEN}" ] && AUTH_HEADER="Authorization: Bearer ${RAZEE_USER_TOKEN}" || AUTH_HEADER="no-auth-available: asdf"
 
@@ -20,7 +20,7 @@ curl \
 -H "Content-Type: application/json" \
 -H "org-id: ${RAZEE_ORG_ID}" \
 -w "HTTP: %{http_code}" \
-${RAZEE_REST_URL}/${TYPE}s/${ID}/${CHILD_TYPE}s/${CHILD_ID}
+${RAZEE_REST_URL}/${TYPE}s/${ID}/${CHILD_TYPE}s${CHILD_ID}
 
 retVal=$?
 
