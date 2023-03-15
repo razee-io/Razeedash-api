de7f25b07ceb93c0baf28706e47f612cb25ae2e3
Ethan St John <42824212+ethanstjohn@users.noreply.github.com>
2023-03-15 09:35:29 -0500

    Update local-dev/api/subSetVersion.sh
    Co-authored-by: carrolp <carrolp@us.ibm.com>

--- local-dev/api/subSetVersion.sh
+++ local-dev/api/subSetVersion.sh
@@ -2,9 +2,9 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-RAZEE_SUB_UUID=${2:-${RAZEE_SUB_UUID:-pSubUuid}}
-RAZEE_ORG_ID=${3:-${RAZEE_ORG_ID:-pOrgId}}
-RAZEE_VER_UUID=${6:-${RAZEE_VER_UUID:-pVerUuid}}
+RAZEE_SUB_UUID=${1:-${RAZEE_SUB_UUID:-pSubUuid}}
+RAZEE_ORG_ID=${2:-${RAZEE_ORG_ID:-pOrgId}}
+RAZEE_VER_UUID=${3:-${RAZEE_VER_UUID:-pVerUuid}}
 
 RAZEE_QUERY='mutation($orgId: String!, $uuid: String!, $versionUuid: String!) { setSubscription( orgId: $orgId, uuid: $uuid, versionUuid: $versionUuid ) { uuid success } }'
 RAZEE_VARIABLES='{"orgId":"'"${RAZEE_ORG_ID}"'","uuid":"'"${RAZEE_SUB_UUID}"'","versionUuid":"'"${RAZEE_VER_UUID}"'"}'
