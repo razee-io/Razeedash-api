3c3e6a5b01289420a428d84abd45e14836300dd7
Ethan St John <42824212+ethanstjohn@users.noreply.github.com>
2023-03-15 09:34:58 -0500

    Update local-dev/api/clusterEnableRegistrationUrl.sh
    Co-authored-by: carrolp <carrolp@us.ibm.com>

--- local-dev/api/clusterEnableRegistrationUrl.sh
+++ local-dev/api/clusterEnableRegistrationUrl.sh
@@ -8,6 +8,6 @@ RAZEE_ORG_ID=${2:-${RAZEE_ORG_ID:-pOrgId}}
 RAZEE_QUERY='mutation($orgId: String! $clusterId: String!) { enableRegistrationUrl( orgId: $orgId clusterId: $clusterId) { url } }'
 RAZEE_VARIABLES='{"clusterId":"'"${RAZEE_CLUSTER_UUID}"'","orgId":"'"${RAZEE_ORG_ID}"'"}'
 
-echo "" && echo "UPDATE registration url"
+echo "" && echo "ENABLE registration url"
 ${SCRIPT_DIR}/graphqlPost.sh "${RAZEE_QUERY}" "${RAZEE_VARIABLES}" | jq --color-output
 echo "Result: $?"
