b0d6d6b40073e873241058302bdfd1d04a6fb571
carrolp <carrolp@us.ibm.com>
2023-05-15 13:49:42 -0400

    Update local-dev/api/apiResourceCreate.sh
    Co-authored-by: Adam King <rak@us.ibm.com>

--- local-dev/api/apiResourceCreate.sh
+++ local-dev/api/apiResourceCreate.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 RAZEE_CLUSTER_UUID=${1:-${RAZEE_CLUSTER_UUID:-pTestClusterUuid}}
 RAZEE_ORG_KEY=${2:-${RAZEE_ORG_KEY:-pOrgKey}}
 
-RAZEE_V2_URL=${RAZEE_V2_URL:-http://localhost:3333/api/v2}
+RAZEE_V2_URL="${RAZEE_V2_URL:-http://localhost:3333/api/v2}"
 
 echo "POST one resource to ${RAZEE_V2_URL}/clusters/${RAZEE_CLUSTER_UUID}/resources"
 curl \
