d2d8ec4a0fa06cda61215fd88185ee1087d54838
Ethan St John <42824212+ethanstjohn@users.noreply.github.com>
2023-07-24 13:14:34 -0500

    Update app/apollo/resolvers/channel.js
    Co-authored-by: carrolp <carrolp@us.ibm.com>

--- app/apollo/resolvers/channel.js
+++ app/apollo/resolvers/channel.js
@@ -118,7 +118,7 @@ const channelResolvers = {
 
         const channels = await models.Channel.find({ org_id, name }).limit(2);
         const channel = channels[0] || null;
-        logger.info({req_id, user, org_id, name}, `${queryName} validating - found: ${channel.length}`);
+        logger.info({req_id, user, org_id, name}, `${queryName} validating - found: ${channels.length}`);
 
         const identifiers = channel ? [channel.uuid, name] : [name];
         await validAuth(me, org_id, ACTIONS.READ, TYPES.CHANNEL, queryName, context, identifiers);
